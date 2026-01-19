8b8e8f52b2637a89ef2cf2f50a8651a96f2c4db5
anakteleg2-svg <anakteleg2@gmail.com>
2026-01-20 03:21:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,21 +8,6 @@ editor.addEventListener("input", () => {
   localStorage.setItem("note", editor.innerHTML);
 });
 
-/* ===== TOOLBAR AUTO HIDE ===== */
-let lastScroll = 0;
-
-window.addEventListener("scroll", () => {
-  const current = window.scrollY;
-
-  if (current > lastScroll && current > 50) {
-    toolbar.style.transform = "translateY(-100%)";
-  } else {
-    toolbar.style.transform = "translateY(0)";
-  }
-
-  lastScroll = current;
-});
-
 /* ===== SERVICE WORKER REGISTER ===== */
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("sw.js");
